0fcdbea8825ef3f25782237ff52aa02f282d5ba1
Trung Le <trung.le@ruby-journal.com>
2020-05-09 02:28:13 +1000

    Update username/password

--- vagrant/bootstrap_kmaster.sh
+++ vagrant/bootstrap_kmaster.sh
@@ -8,19 +8,19 @@ kubeadm init --cri-socket /run/crio/crio.sock --apiserver-advertise-address=172.
 
 # Copy Kube admin config
 echo "Copy kube admin config to user .kube directory"
-mkdir /home/ubuntu/.kube
-cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-chown -R ubuntu:ubuntu /home/ubuntu/.kube
+mkdir /home/vagrant/.kube
+cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown -R vagrant:vagrant/home/vagrant/.kube
 
 # Deploy Calico network
 echo "Deploy Calico network"
 cd /tmp && wget https://docs.projectcalico.org/manifests/calico.yaml
 # issue: https://github.com/projectcalico/calico/issues/3455
 sed -i 's/v3\.13\.3/v3\.12\.1/g' /tmp/calico.yaml
-chown ubuntu:ubuntu /tmp/calico.yaml
-su - ubuntu -c "kubectl create -f /tmp/calico.yaml"
+chown vagrant:vagrant/tmp/calico.yaml
+su - vagrant -c "kubectl create -f /tmp/calico.yaml"
 
 # Generate Cluster join command
 echo "Generate and save cluster join command to /joincluster.sh"
-kubeadm token create --print-join-command > /home/ubuntu/join_cluster.sh
-chown ubuntu:ubuntu /home/ubuntu/join_cluster.sh
+kubeadm token create --print-join-command > /home/vagrant/join_cluster.sh
+chown vagrant:vagrant/home/vagrant/join_cluster.sh
