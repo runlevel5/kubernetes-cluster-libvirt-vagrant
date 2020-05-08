b75774728b82a6384c670bf88cb08e917300c333
Trung Le <trung.le@ruby-journal.com>
2020-05-09 01:35:52 +1000

    Switch to Debian 10

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,67 @@
 #!/bin/bash
 set -e
 
+# Install CRI-O
+echo "[CRI] Installing CRI-O"
+modprobe overlay
+modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_10/ /'  >> /etc/apt/sources.list.d/libcontainers.list
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/Debian_10/Release.key -O- | sudo apt-key add -
+apt-get update && apt-get install -y cri-o-1.17
+
+systemctl daemon-reload
+systemctl start crio
+
+
+# Install kubelet
+apt-get install -y socat ebtables ethtool conntrack
+cd /tmp
+wget https://github.com/runlevel5/kubernetes-packages/blob/master/debian-10/ppc64le/kubernetes-cni_0.7.5-0_ppc64el.deb
+dpkg -i kubernetes-cni_0.7.5-0_ppc64el.deb
+wget https://github.com/runlevel5/kubernetes-packages/blob/master/debian-10/ppc64le/kubelet_1.18.2-0_ppc64el.deb
+dpkg -i kubelet_1.18.2-0_ppc64el.deb
+cat << EOF | tee /etc/default/kubelet
+KUBELET_EXTRA_ARGS="--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/crio/crio.sock"
+EOF
+systemctl daemon-reload
+systemctl start kubelet
+rm /tmp/kubernetes-cni_0.7.5-0_ppc64el.deb
+rm /tmp/kubelet_1.18.2-0_ppc64el.deb
+
+# Install kubectl
+cd /tmp
+wget https://github.com/runlevel5/kubernetes-packages/blob/master/debian-10/ppc64le/kubectl_1.18.2-0_ppc64el.deb
+dpkg -i kubectl_1.18.2-0_ppc64el.deb
+rm /tmp/kubectl_1.18.2-0_ppc64el.deb
+
+# Install critool
+cd /tmp
+wget https://github.com/runlevel5/kubernetes-packages/blob/master/debian-10/ppc64le/cri-tools_1.18.0-0_ppc64el.deb
+dpkg -i cri-tools_1.18.0-0_ppc64el.deb
+cat << EOF | tee  /etc/crictl.yaml
+runtime-endpoint: unix:///run/crio/crio.sock
+image-endpoint: unix:///run/crio/crio.sock
+timeout: 10
+debug: true
+EOF
+rm /tmp/cri-tools_1.18.0-0_ppc64el.deb
+
+# Install kubeadm
+cd /tmp
+wget https://github.com/runlevel5/kubernetes-packages/blob/master/debian-10/ppc64le/kubeadm_1.18.2-0_ppc64el.deb
+dpkg -i kubeadm_1.18.2-0_ppc64el.deb
+rm /tmp/kubeadm_1.18.2-0_ppc64el.deb
+
 # Update hosts file
 echo "[TASK 1] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
@@ -9,3 +70,6 @@ cat >>/etc/hosts<<EOF
 172.42.42.102 kworker2.example.com kworker2
 EOF
 
+# Disable swap
+sed -i '/swap/d' /etc/fstab
+swapoff -a
--- vagrant/bootstrap_kmaster.sh
+++ vagrant/bootstrap_kmaster.sh
@@ -2,18 +2,18 @@
 set -e
 
 # Initialize Kubernetes
-echo "[TASK 13] Initialize Kubernetes Cluster"
-apt-get install conntrack socat
-kubeadm init --cri-socket /run/containerd/containerd.sock --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 
+echo "Initialize Kubernetes Cluster"
+kubeadm init --cri-socket /run/crio/crio.sock --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 
+
 
 # Copy Kube admin config
-echo "[TASK 14] Copy kube admin config to user .kube directory"
+echo "Copy kube admin config to user .kube directory"
 mkdir /home/ubuntu/.kube
 cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 chown -R ubuntu:ubuntu /home/ubuntu/.kube
 
 # Deploy Calico network
-#echo "[TASK 15] Deploy Calico network"
+echo "Deploy Calico network"
 cd /tmp && wget https://docs.projectcalico.org/manifests/calico.yaml
 # issue: https://github.com/projectcalico/calico/issues/3455
 sed -i 's/v3\.13\.3/v3\.12\.1/g' /tmp/calico.yaml
@@ -21,6 +21,6 @@ chown ubuntu:ubuntu /tmp/calico.yaml
 su - ubuntu -c "kubectl create -f /tmp/calico.yaml"
 
 # Generate Cluster join command
-echo "[TASK 16] Generate and save cluster join command to /joincluster.sh"
+echo "Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /home/ubuntu/join_cluster.sh
 chown ubuntu:ubuntu /home/ubuntu/join_cluster.sh
