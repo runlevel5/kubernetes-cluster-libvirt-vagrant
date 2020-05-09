9468e3038d98e77351a33f58c3f6c2712e17818f
Trung Le <trung.le@ruby-journal.com>
2020-05-09 20:56:35 +1000

    Update vagrant
    * Use open-power/ubuntu-18.04-ppc64le image
    * Install k8s packages in bootstrap script

--- vagrant/bootstrap_kworker.sh
+++ vagrant/bootstrap_kworker.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Join worker nodes to the Kubernetes cluster
-echo "[TASK 13] Join node to Kubernetes Cluster"
+echo "Join node to Kubernetes Cluster"
 
 apt-get install -q -y sshpass
-sshpass -p "ubuntu" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@kmaster.example.com:/home/ubuntu/join_cluster.sh /home/ubuntu/join_cluster.sh
-bash /home/ubuntu/join_cluster.sh
+sshpass -p "vagrant" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@kmaster.example.com:/home/vagrant/join_cluster.sh /home/vagrant/join_cluster.sh
+bash /home/vagrant/join_cluster.sh
